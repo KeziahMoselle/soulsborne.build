f66736684318986d83d81bba9ce1542a81d08531
KeziahMoselle <contact@keziahmoselle.fr>
2024-04-04 16:59:15 +0200

    fix(frontend): middleware no cookie

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,16 +2,14 @@ import type { PayloadUserResponse } from "@/types";
 import { defineMiddleware } from "astro:middleware"
 
 export const onRequest = defineMiddleware(async ({ cookies, locals }, next) => {
-  const payloadToken = cookies.get('payload-token').value
-
-  if (!payloadToken) {
+  if (!cookies.has('payload-token')) {
     locals.user = null
     return next()
   }
 
   const data: PayloadUserResponse = await fetch(`${import.meta.env.PUBLIC_PAYLOAD_URL}/api/users/me`, {
     headers: {
-      'Cookie': `payload-token=${payloadToken}`
+      'Cookie': `payload-token=${cookies.get('payload-token')}`
     },
   }).then((res) => res.json())
 
