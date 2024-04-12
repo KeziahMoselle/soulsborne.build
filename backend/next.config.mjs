8127ab0d57c91756a163f85ae8b073e42fbfe434
KeziahMoselle <contact@keziahmoselle.fr>
2024-04-12 11:06:52 +0200

    chore: ignore ts errors on build

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -2,7 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 }
 
 export default withPayload(nextConfig)
