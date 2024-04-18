63c3542c83a197e8408690311b8dd40210337dc3
KeziahMoselle <contact@keziahmoselle.fr>
2024-04-18 10:11:39 +0200

    chore: add sentry

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 import cloudflare from '@astrojs/cloudflare'
+import sentry from '@sentry/astro'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'viewport'
+    defaultStrategy: 'viewport',
   },
   adapter: cloudflare({
     platformProxy: {
@@ -21,6 +22,13 @@ export default defineConfig({
     }),
     tailwind({
       applyBaseStyles: false,
+    }),
+    sentry({
+      dsn: "https://d174fc3d55895a8efab55a1671c3a12b@o4507102601084928.ingest.de.sentry.io/4507102605475920",
+      sourceMapsUploadOptions: {
+        project: "frontend",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
     }),
   ],
   devToolbar: {
