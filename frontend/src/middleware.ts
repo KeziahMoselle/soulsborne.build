e2a9a4f4634f83c65ab048b085590ab2ff47c257
KeziahMoselle <contact@keziahmoselle.fr>
2024-04-22 18:43:17 +0200

    feat(beta): restrict access to non logged users

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -7,6 +7,7 @@ function parseJwt(token) {
   return JSON.parse(atob(base64));
 }
 
+const AUHTORIZED_BETA_PATHNAMES = ['/']
 
 const auth = defineMiddleware(async ({ cookies, locals }, next) => {
   try {
@@ -20,4 +21,20 @@ const auth = defineMiddleware(async ({ cookies, locals }, next) => {
   }
 })
 
-export const onRequest = sequence(auth)
+const beta = defineMiddleware(async ({ locals, url, redirect }, next) => {
+  try {
+    if (locals.user) {
+      return next()
+    }
+
+    if (AUHTORIZED_BETA_PATHNAMES.includes(url.pathname)) {
+      return next()
+    }
+
+    return redirect('/', 307)
+  } catch {
+    return redirect('/', 307)
+  }
+})
+
+export const onRequest = sequence(auth, beta)
